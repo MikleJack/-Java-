776ee9f2d7910fad717c8c938d2e13b744ebdd8a
liang.chenhong <1339438912@qq.com>
2022-01-05 11:08:38 +0800

    管理员工单查询界面

--- 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/admin/adminOerderSearch.vue
+++ 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/admin/adminOerderSearch.vue
@@ -26,36 +26,43 @@
     </el-form>
 
     <!--    管理员表单查询-->
+
+
     <el-table
+      border
       style="width: 100%"
-      :data="tableData.filter(data => !search || data.workOrderType.equals(workOrderTypeSelector) ||
-      data.workOrderName.toLowerCase().includes(search.toLowerCase())).slice((currentPage-1)*pageSize,currentPage*pageSize)"
-      element-loading-background="rgba(245, 247, 250, 1)">
+      :data = "tableData">
+
+<!--      接口调用数据-->
+<!--      :data="tableData.filter(data => !search || data.workOrderType.equals(workOrderTypeSelector) ||-->
+<!--      data.workOrderName.toLowerCase().includes(search.toLowerCase())).slice((currentPage-1)*pageSize,currentPage*pageSize)"-->
+<!--      element-loading-background="rgba(245, 247, 250, 1)">-->
       <el-table-column
+        prop="worker_num"
         label="工号"
-        width="250">
+        width="150">
 <!--        <template slot="header" slot-scope="scope">-->
 
 <!--        </template>-->
         <template slot-scope="scope">
-          <!--          文字链接，当点击项目名称后将跳转到该项目的考核页面-->
+          <!--          文字链接，当点击项目名称后将跳x  转到该项目的考核页面-->
           <el-link :underline="false" type="primary" @click="go(scope.row.workOrderNum)">{{ scope.row.workOrderNum }}</el-link>
         </template>
       </el-table-column>
       <el-table-column
         prop="workOrderType"
         label="工单类型"
-        width="250">
+        width="150">
       </el-table-column>
       <el-table-column
-        width="250"
+        width="180"
         prop="workOrderName"
         label="工单标题">
       </el-table-column>
       <el-table-column
         prop="applicant"
         label="申请人"
-        width="250">
+        width="150">
       </el-table-column>
       <el-table-column
         prop="applyTime"
@@ -65,12 +72,12 @@
       <el-table-column
         prop="workOrderState"
         label="工单状态"
-        width="250">
+        width="180">
       </el-table-column>
       <el-table-column
         prop="workOrderState"
         label="项目状态"
-        width="340">
+        width="180">
         <template slot-scope="scope" >
           <div v-if="scope.row.projectStatus===1">
             已结项
@@ -80,7 +87,31 @@
           </div>
         </template>
       </el-table-column>
+
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="250">
+        <templte slot-scope="scope">
+          <el-button @click="handleClick_detail(scope.row)" type="text" size="small">详情</el-button>
+        </templte>
+
+      </el-table-column>
     </el-table>
+
+<!--    点击详情后的dialog界面-->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible_detail"
+      width="30%"
+      :before-close="handleClose">
+      <span>是否确定重置该用户账号？</span>
+      <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible_detail = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible_detail = false">确 定</el-button>
+              </span>
+    </el-dialog>
+
     <div>
       <el-pagination
         @current-change="handleCurrentChange"
@@ -98,13 +129,29 @@ export default {
   name: "ticketSearch",
   data() {
     return {
-      tableData: [],
+      dialogVisible_detail: false,
       ticketData: [],
       currentPage:1,
       pageSize:8,
       search:'',
-
-      workOrderTypeSelector:''
+      workOrderTypeSelector:'',
+      tableData: [
+        {
+        worker_num: '00000001',
+        name: '王小虎',
+        phone: '15155185464',
+        dep_name: '软件学院',
+        dep_level: '3',
+        state: '正常'
+      }, {
+        worker_num: '00000002',
+        name: '王小虎',
+        phone: '15155185464',
+        dep_name: '软件学院',
+        dep_level: '3',
+        state: '正常'
+      }
+      ]
     }
   },
   mounted() {
@@ -113,6 +160,20 @@ export default {
       this.tableData = res.data;
     })
   },
+  methods: {
+    //操作的详情dialog函数
+    handleClick_detail() {
+      this.dialogVisible_detail = true;
+    },
+    //dialog弹窗关闭提示函数
+    handleClose(done) {
+      // this.$confirm('确认关闭？')
+      // .then(_ => {
+      done();
+      // })
+      // .catch(_ => {});
+    }
+  }
 }
 
 
