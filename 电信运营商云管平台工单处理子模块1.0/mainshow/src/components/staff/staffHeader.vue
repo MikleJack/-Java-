f202fb617b2cb0d9d331f685b9c9051e6112e5e2
fhj <f_mailbox@163.com>
2022-01-06 11:44:59 +0800

    用户申请界面bug

--- 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/staff/staffHeader.vue
+++ 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/staff/staffHeader.vue
@@ -2,7 +2,7 @@
   <el-container class="container">
     <!-- 上方导航栏 -->
     <el-header class="header">
-      <div style="position:relative;float: left;top: 5%;">
+      <div style="position:relative;float: left;top: 5px;">
         <img src="../../assets/logo.png" height="40" width="40"/>
       </div>
       <div style="position:relative;float: left;top: 13px; width:400px;font-size:20px;">
@@ -22,9 +22,9 @@
     </el-header>
     <!-- 侧边导航栏 -->
     <el-container>
-      <el-aside class="aside" width=15%>
+      <el-aside class="aside" width=150px>
         <el-menu
-          default-active="2"
+          default-active="this.$route.path"
           class="el-menu-vertical-demo"
           @open="handleOpen"
           @close="handleClose"
@@ -32,69 +32,143 @@
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-submenu index="1">
+          <el-menu-item index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>首页</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title"></template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
+          </el-menu-item>
 
-          </el-submenu>
-          <el-submenu index="2">
+          <el-menu-item index="2" >
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航二</span>
+              <span>申请工单</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title"></template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
+          </el-menu-item>
 
-          </el-submenu>
-          <el-submenu index="3">
+          <el-menu-item index="3">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航三</span>
+              <span>变更工单</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title"></template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
-            </el-menu-item-group>
-
-          </el-submenu>
-          <el-submenu index="4">
+          </el-menu-item>
+          <el-menu-item index="4">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航四</span>
+              <span>全部工单列表</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title"></template>
-              <el-menu-item index="4-1">选项1</el-menu-item>
-              <el-menu-item index="4-2">选项2</el-menu-item>
-            </el-menu-item-group>
-
-          </el-submenu>
+          </el-menu-item>
         </el-menu>
       </el-aside>
       <!-- main区域 -->
       <el-main class="main">
+        <!-- <changetable></changetable> -->
+<!--        <changetable></changetable>-->
         <employee-portal></employee-portal>
+        <!-- <changepassword></changepassword> -->
       </el-main>
     </el-container>
   </el-container>
+<!--  <el-container class="container">-->
+<!--    &lt;!&ndash; 上方导航栏 &ndash;&gt;-->
+<!--    <el-header class="header">-->
+<!--      <div style="position:relative;float: left;top: 5%;">-->
+<!--        <img src="../../assets/logo.png" height="40" width="40"/>-->
+<!--      </div>-->
+<!--      <div style="position:relative;float: left;top: 13px; width:400px;font-size:20px;">-->
+<!--        电信运营商云管平台工单处理子模块-->
+<!--      </div>-->
+
+<!--      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" text-color="black">-->
+<!--        <el-submenu index="2" >-->
+<!--          <template slot="title">个人中心</template>-->
+<!--          <el-menu-item index="2-1" >选项1</el-menu-item>-->
+<!--          <el-menu-item index="2-2">选项2</el-menu-item>-->
+<!--          <el-menu-item index="2-3">选项3</el-menu-item>-->
+<!--        </el-submenu>-->
+<!--        <el-menu-item index="2" @click="logoutFn">-->
+<!--          退出登录 </el-menu-item>-->
+<!--      </el-menu>-->
+<!--    </el-header>-->
+<!--    &lt;!&ndash; 侧边导航栏 &ndash;&gt;-->
+<!--    <el-container>-->
+<!--      <el-aside class="aside" width=15%>-->
+<!--        <el-menu-->
+<!--          default-active="2"-->
+<!--          class="el-menu-vertical-demo"-->
+<!--          @open="handleOpen"-->
+<!--          @close="handleClose"-->
+<!--          background-color="#52b69a"-->
+<!--          text-color="#fff"-->
+<!--          active-text-color="#ffd04b"-->
+<!--        >-->
+<!--          <el-submenu index="1">-->
+<!--            <template slot="title">-->
+<!--              <i class="el-icon-location"></i>-->
+<!--              <span>导航一</span>-->
+<!--            </template>-->
+<!--            <el-menu-item-group>-->
+<!--              <template slot="title"></template>-->
+<!--              <el-menu-item index="1-1">选项1</el-menu-item>-->
+<!--              <el-menu-item index="1-2">选项2</el-menu-item>-->
+<!--            </el-menu-item-group>-->
+
+<!--          </el-submenu>-->
+<!--          <el-submenu index="2">-->
+<!--            <template slot="title">-->
+<!--              <i class="el-icon-location"></i>-->
+<!--              <span>导航二</span>-->
+<!--            </template>-->
+<!--            <el-menu-item-group>-->
+<!--              <template slot="title"></template>-->
+<!--              <el-menu-item index="2-1">选项1</el-menu-item>-->
+<!--              <el-menu-item index="2-2">选项2</el-menu-item>-->
+<!--            </el-menu-item-group>-->
+
+<!--          </el-submenu>-->
+<!--          <el-submenu index="3">-->
+<!--            <template slot="title">-->
+<!--              <i class="el-icon-location"></i>-->
+<!--              <span>导航三</span>-->
+<!--            </template>-->
+<!--            <el-menu-item-group>-->
+<!--              <template slot="title"></template>-->
+<!--              <el-menu-item index="3-1">选项1</el-menu-item>-->
+<!--              <el-menu-item index="3-2">选项2</el-menu-item>-->
+<!--            </el-menu-item-group>-->
+
+<!--          </el-submenu>-->
+<!--          <el-submenu index="4">-->
+<!--            <template slot="title">-->
+<!--              <i class="el-icon-location"></i>-->
+<!--              <span>导航四</span>-->
+<!--            </template>-->
+<!--            <el-menu-item-group>-->
+<!--              <template slot="title"></template>-->
+<!--              <el-menu-item index="4-1">选项1</el-menu-item>-->
+<!--              <el-menu-item index="4-2">选项2</el-menu-item>-->
+<!--            </el-menu-item-group>-->
+
+<!--          </el-submenu>-->
+<!--        </el-menu>-->
+<!--      </el-aside>-->
+<!--      &lt;!&ndash; main区域 &ndash;&gt;-->
+<!--      <el-main class="main">-->
+<!--&lt;!&ndash;        <employee-portal></employee-portal>&ndash;&gt;-->
+<!--&lt;!&ndash;        <changepassword></changepassword>&ndash;&gt;-->
+<!--        <changetable></changetable>-->
+<!--      </el-main>-->
+<!--    </el-container>-->
+<!--  </el-container>-->
 </template>
 
 <script>
 import EmployeePortal from "./employeePortal";
+import Changepassword from "../changepassword";
+import Changetable from "./changetable";
 export default {
   name: "header",
-  components: {EmployeePortal},
+  components: {Changetable, Changepassword, EmployeePortal},
   methods: {
     //退出
     logoutFn() {
