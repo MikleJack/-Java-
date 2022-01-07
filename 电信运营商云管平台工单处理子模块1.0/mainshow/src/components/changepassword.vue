c56921cbf7cac60c194b4c6df375d09df7d3dde5
liangyun <2077283547@qq.com>
2022-01-07 10:13:27 +0800

    个人信息及密码修改界面，格式及代码修整

--- 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/changepassword.vue
+++ 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/changepassword.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="all-container">
     <div class="all-container-padding bg" >
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane label="基本信息" name="first">
+<!--      修改个人信息表单-->
+      <el-tabs v-model="activeName" @tab-click="handleClick" >
+<!--        上方导航栏-->
+        <el-tab-pane label="基本信息" name="first" style="margin-left: 30%">
           <el-form :model="userlist" :rules="rules" ref="EditorUserForms">
             <el-form-item label="用户名" prop="username" :label-width="formLabelWidth">
               <el-col :span="8">  <el-input v-model="userlist.username" disabled ></el-input></el-col>
@@ -18,13 +20,14 @@
             <el-form-item label="电话" prop="phone" :label-width="formLabelWidth">
               <el-col :span="8">   <el-input v-model="userlist.phone" placeholder="请输入电话"></el-input></el-col>
             </el-form-item>
-
+<!--保存-->
           </el-form>
-          <div class="grid-content bg-purple">
+          <div class="grid-content bg-purple" style="margin-left: 20%">
             <el-button type="primary"  @click="EditorUserClick('userlist')" >保存</el-button>
           </div>
+<!--        修改密码表单-->
         </el-tab-pane>
-        <el-tab-pane label="修改密码" name="second">
+        <el-tab-pane label="修改密码" name="second" style="margin-left: 30%">
           <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
             <el-form-item label="原密码" prop="pass" :label-width="formLabelWidth">
               <el-col :span="8">   <el-input v-model="ruleForm.pass" placeholder="请输入原密码" type="password"></el-input></el-col>
@@ -36,7 +39,8 @@
               <el-col :span="8">   <el-input v-model="ruleForm.checknewpass" placeholder="请再次输入新密码" id='newkey1' type="password"></el-input></el-col>
             </el-form-item>
           </el-form>
-          <div class="grid-content bg-purple">
+<!--          保存-->
+          <div class="grid-content bg-purple" style="margin-left: 20%">
             <el-button type="primary"  @click="submitForm('ruleForm')">保存</el-button>
           </div>
         </el-tab-pane>
@@ -82,7 +86,7 @@ export default {
             message: "请输入电话号码"
           },
           {
-            pattern: /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/,
+            pattern: /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/,//确保输入为13位数字
             message: "手机格式不对"
           }
         ],
@@ -93,73 +97,32 @@ export default {
             message: "请输入密码"
           }
         ],
-
         newpass: [
           {
-            validator: validatePass,
+            validator: validatePass,//用来校验新密码是否重复输入正确
             trigger: "blur"
           }
         ],
         checknewpass: [
           {
-            validator: validatePass2,
+            validator: validatePass2,//用来校验新密码是否重复输入正确
             trigger: "blur"
           }
         ]
       }
     };
   },
-
-
   methods: {
     //tab切换
     handleClick(tab, event) {
       console.log(tab, event);
     },
-    //修改密码
+    //保存新密码
     submitForm(ruleForm) {
-      var obj = {
-        username: this.username,
-        oldpwd: this.ruleForm.pass,
-        newpwd: this.ruleForm.newpass
-      };
-      console.log(obj);
-      postData("接口", obj).then(response => {
-        if (response.status == 200) {
-          this.$message({
-            message: "保存成功",
-            type: "success"
-          });
-        } else {
-          this.$message({
-            message: "修改失败" + response.message,
-            type: "error"
-          });
-        }
-      });
     },
-    // 编辑提交的方法
+    //保存个人信息
     EditorUserClick() {
-      this.$refs.EditorUserForms.validate(valid => {
-        if (valid) {
-          console.log(this.userlist);
-          putData("接口", this.userlist).then(response => {
-            if (response.status == 200) {
-              this.$message({
-                message: "编辑成功",
-                type: "success"
-              });
-            } else {
-              this.$message({
-                message: "修改失败" + response.message,
-                type: "error"
-              });
-            }
-          });
-
-        }
-      });
     }
   }
-};
+}
 </script>
