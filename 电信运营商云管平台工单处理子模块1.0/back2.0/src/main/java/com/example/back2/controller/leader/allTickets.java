d8336918a8baeaa77e88ad24c950c42fc6d131d4
lhm <1076252778@qq.com>
2022-01-10 21:17:30 +0800

    搜索所有工单

--- 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/controller/leader/allTickets.java
+++ 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/controller/leader/allTickets.java
@@ -1,4 +1,27 @@
 package com.example.back2.controller.leader;
 
+
+import com.example.back2.entity.Leaderworkorderall;
+import com.example.back2.service.LeaderworkorderallService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("leader")
 public class allTickets {
+
+    //--------------查询所有工单信息----------------------
+    @Resource
+    private LeaderworkorderallService leaderworkorderallService;
+
+    @GetMapping("selectAll")
+    public List<Leaderworkorderall> selectAll(){
+        return this.leaderworkorderallService.selectAll();
+    }
+
+
 }
