541dd1f4abe00c7e59359cb23eaa6804dde519e3
lhm <1076252778@qq.com>
2022-01-11 16:34:29 +0800

    查询所有虚拟机配置，所有未分配的物理机

--- 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/controller/staff/applyTicket.java
+++ 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/controller/staff/applyTicket.java
@@ -2,13 +2,19 @@ package com.example.back2.controller.staff;
 
 import com.example.back2.entity.table.PhysicsComResource;
 import com.example.back2.entity.table.VmSpecifications;
+import com.example.back2.entity.table.WorkOrder;
 import com.example.back2.service.table.PhysicsComResourceService;
 import com.example.back2.service.table.VmSpecificationsService;
+import com.example.back2.service.table.WorkOrderService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,6 +24,8 @@ public class applyTicket {
     private PhysicsComResourceService physicsComResourceService;
     @Resource
     private VmSpecificationsService vmSpecificationsService;
+    @Resource
+    private WorkOrderService workOrderService;
 
     @GetMapping("selectAllpc")
     public List<PhysicsComResource> selectAllpc(){
@@ -28,4 +36,27 @@ public class applyTicket {
     public List<VmSpecifications> selectAllvm(){
         return this.vmSpecificationsService.selectAllvm();
     }
+
+    @PostMapping("intsertApplyTicket")
+    public boolean intsertApplyTicket(String workOrderName,String expirationTime,String reason){
+        Date d = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+        String workOrderNum = df.format(d);
+        double t1 = Math.random();
+        t1 *= 10000;
+        int t2 = (int) t1;
+        workOrderNum += t2+ "";
+//        System.out.println(workOrderNum);
+
+
+
+
+        WorkOrder workOrder = new WorkOrder();
+        workOrder.setWorkOrderNum(workOrderNum);
+        workOrder.setWorkOrderName(workOrderName);
+
+        workOrder.setReason(reason);
+        return true;
+    }
+
 }
