c273eec72f1b699752f11b32d8bab3b712fe3168
zhengshaojie <1842839572@qq.com>
2022-01-11 18:32:06 +0800

    管理员 工单查询界面 时间格式修改

--- 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/entity/view/AdminsearchorderTable.java
+++ 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/entity/view/AdminsearchorderTable.java
@@ -1,5 +1,8 @@
 package com.example.back2.entity.view;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.Serializable;
@@ -23,7 +26,8 @@ public class AdminsearchorderTable implements Serializable {
 
     private String workOrderState;
 
-    private String expirationTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date expirationTime;
 
     private String name;
 
@@ -68,13 +72,14 @@ public class AdminsearchorderTable implements Serializable {
         this.workOrderState = workOrderState;
     }
 
-    public String getExpirationTime() {
+    public Date getExpirationTime() {
         return expirationTime;
     }
 
     public void setExpirationTime(Date expirationTime) {
-        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
-        this.expirationTime = ft.format(expirationTime);
+        this.expirationTime = expirationTime;
+//        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+//        this.expirationTime = ft.format(expirationTime);
     }
 
     public String getName() {
