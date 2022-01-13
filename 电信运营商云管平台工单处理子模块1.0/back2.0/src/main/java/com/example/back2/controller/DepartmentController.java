fd468215b42d1bda525f24219be6bc6693d85c64
fhj <f_mailbox@163.com>
2022-01-13 14:46:34 +0800

    增加获取部门预算接口

---
+++ 电信运营商云管平台工单处理子模块1.0/back2.0/src/main/java/com/example/back2/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.example.back2.controller;
+
+import com.example.back2.entity.table.Department;
+import com.example.back2.service.table.DepartmentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("depart")
+public class DepartmentController {
+
+    @Resource
+    private DepartmentService departmentService;
+
+    /**
+     *
+     * @param depNum 部门
+     * @return 获取该部门预算
+     */
+    @GetMapping("getDepBudget")
+    public Integer getDepBudget(Integer depNum){
+        Department department = departmentService.queryById(depNum);
+        return department.getDepBudget();
+    }
+}
