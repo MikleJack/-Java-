b212f800c21e1805e700356da9f6f5c02465eeef
lhm <1076252778@qq.com>
2022-01-19 18:09:34 +0800

    重构了管理员首页

---
+++ 电信运营商云管平台工单处理子模块1.0/mainshow/src/components/admin/dialogs/Depbudget.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-dialog
+    style="text-align: center"
+    title="部门预算设置"
+    :visible.sync="this.$store.state.dialogVisible_budget"
+    width="500px"
+    height="700px"
+    :before-close="handleClose">
+    <div class="block" style="text-align: center">
+      <el-table class="budTable" frame=above
+                :data="tableData_bud"
+                border
+                style="width: 100%"
+                :cell-style="{textAlign:'center'}">
+        <el-table-column prop="depName" label="部门名称" width="180" align="center">
+        </el-table-column>
+        <el-table-column prop="depBudget" label="预算(元)" width="278.5" align="center">
+          <template slot-scope="scope">
+            <el-input-number v-model="scope.row.depBudget" controls-position="right" @change="handleChange_bud(scope.row.depBudget)"
+                             :step="100" :min="0" :max="999999"
+                             style="margin-left: 8%" size="mini"></el-input-number>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <span slot="footer" class="dialog-footer">
+                <el-button @click="$store.state.dialogVisible_budget = false">取 消</el-button>
+                <el-button style="margin-right: 32%;color: white;background-color: #52b69a " @click="budget_confirm()">确 定</el-button>
+      </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "Depbudget",
+
+
+  data(){
+    return{
+      tableData_bud: [],
+      // 部门预算使用率
+      depBudget:'78.90',
+    }
+  },
+  methods:{
+
+    //部门预算初始化
+    setbudget(){
+      this.$axios.get(this.$store.state.url+"/adminHome/getDepBudget").then((res)=>{
+        if(res.data!==null){
+          this.tableData_bud = res.data;
+        }
+      })
+      // this.dialogVisible_budget=true;
+    },
+
+    //部门预算设置的dialog函数
+    //关闭隐藏弹窗的函数
+    handleClose(done) {
+      this.$store.state.dialogVisible_budget = false;
+      done();
+    },
+
+    handleChange_bud(budValue) {
+      console.log(value);
+    },
+    //对预算dialog的确认
+    budget_confirm(){
+      this.$store.state.which_page_confirm = 1
+      this.$store.state.dialogVisible_modify = true
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
